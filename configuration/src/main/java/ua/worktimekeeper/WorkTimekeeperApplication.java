0c9761404d283ea1cf1a007bf8c9031efc393133
Serhii <seryj.naumenko@gmail.com>
2019-11-02 21:28:22 +0200

    #1 added module with spring boot plugin

--- configuration/src/main/java/ua/worktimekeeper/WorkTimekeeperApplication.java
+++ configuration/src/main/java/ua/worktimekeeper/WorkTimekeeperApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkTimekeeperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkTimekeeperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTimekeeperApplication.class, args);
+    }
 
 }
